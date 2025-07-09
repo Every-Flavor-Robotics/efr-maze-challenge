15d845b317d48eb86dc45533cd2529049c40680d
Swapnil Pande <swapnil.pande98@gmail.com>
2025-07-09 17:11:31 -0400

    Create Abstract Solver

---
+++ maze_challenge/solver.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+from typing import Dict, List, Set, Tuple
+
+# Define how to move in each direction
+DIRECTION_VECTORS = {
+    "NORTH": (-1, 0),
+    "SOUTH": (1, 0),
+    "EAST": (0, 1),
+    "WEST": (0, -1),
+}
+
+# Get the reverse of a direction for backtracking
+REVERSE_DIRECTION = {
+    "NORTH": "SOUTH",
+    "SOUTH": "NORTH",
+    "EAST": "WEST",
+    "WEST": "EAST",
+}
+
+
+class AbstractSolver(ABC):
+    """Base class for all solvers. Enforces interface consistency."""
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    @abstractmethod
+    def choose_move(
+        self, position: Tuple[int, int], possible_moves: Dict[str, Tuple[int, int]]
+    ) -> str:
+        """
+        Given the current position and possible directions, return a move as a string:
+        One of: "N", "S", "E", "W"
+        """
+        pass
